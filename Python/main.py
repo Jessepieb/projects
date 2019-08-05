e39794091db395677c1a155bfaeac38e58f50291
Jessepieb <jessepiebenga@gmail.com>
2019-08-05 14:06:26 +0200

    up

--- Python/main.py
+++ Python/main.py
@@ -1,37 +1,52 @@
 import smtplib
 import nmap
 
-file = open("input.txt")
-macs = open("macs.txt", "a+")
-creds = file.readlines()
-sent_from = creds[0]
-to = [creds[0]]
-nmScan = nmap.PortScanner()
-subject = "Testmail"
-msg = ""
-nmScan.scan(hosts='192.168.192.1/24', arguments='-sn')
 
-for host in nmScan.all_hosts():
-    if 'mac' in nmScan[host]['addresses']:
-        msg += str(nmScan[host]['addresses']['ipv4']) + ' : ' + str(nmScan[host]['vendor'])
-        if str(nmScan[host]['vendor']) not in macs:
-            macs.write(str(nmScan[host]['vendor']))
-    msg += nmScan[host].hostname() + '\n '
-email_text= """\
-From: %s
-To: %s
-Subject: %s
+def getMacs(iprange):
 
-%s
-""" % (sent_from,", ".join(to), subject, str(msg))
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(creds[0], creds[1])
-    server.sendmail(sent_from, to, email_text)
-    server.close()
-except:
-    print("somthing went wrong")
+    macs = open("macs.txt", "a+")
+    nmScan = nmap.PortScanner()
+    nmScan.scan(hosts=iprange, arguments='-sn')
+    msg = ""
+    for host in nmScan.all_hosts():
+        if 'mac' in nmScan[host]['addresses']:
+            msg += str(nmScan[host]['addresses']['ipv4']) + ' : ' + str(nmScan[host]['vendor'])
+            if str(nmScan[host]['vendor']) not in macs:
+                macs.write(str(nmScan[host]['vendor']))
+        msg += nmScan[host].hostname() + '\n '
 
-file.close()
-macs.close()
+    macs.close()
+    return msg
+
+def sendMail(body):
+
+    file = open("input.txt")
+    creds = file.readlines()
+    sent_from = creds[0]
+    to = [creds[0]]
+    subject = "Testmail"
+    msg = body
+    email_text= """\
+    From: %s
+    To: %s
+    Subject: %s
+    
+    %s
+    """ % (sent_from, ", ".join(to), subject, str(msg))
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(creds[0], creds[1])
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+    except:
+        print("somthing went wrong")
+    file.close()
+
+
+def main():
+    text = getMacs('192.168.192.1/24')
+    sendMail(text)
+
+
+main()
