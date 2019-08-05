3d66769234fbe850fd9c13a6aee30da731b7ef2d
Jessepieb <jessepiebenga@gmail.com>
2019-08-05 14:16:50 +0200

    up

--- Python/main.py
+++ Python/main.py
@@ -24,20 +24,30 @@ def sendMail(body):
     creds = file.readlines()
     sent_from = creds[0]
     to = [creds[0]]
-    subject = "Testmail"
     msg = body
-    email_text= """\
+    email_text = """\
     From: %s
     To: %s
-    Subject: %s
     
     %s
-    """ % (sent_from, ", ".join(to), subject, str(msg))
+    """ % (sent_from, ",".join(to), str(msg))
+
+    message = """From: From Person <from@fromdomain.com>
+    To: To Person <to@todomain.com>
+    MIME-Version: 1.0
+    Content-type: text/html
+    Subject: SMTP HTML e-mail test
+
+    This is an e-mail message to be sent in HTML format
+
+    <b>This is HTML message.</b>
+    <h1>This is headline.</h1>
+    """
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(creds[0], creds[1])
-        server.sendmail(sent_from, to, email_text)
+        server.sendmail(sent_from, to, message)
         server.close()
     except:
         print("somthing went wrong")
