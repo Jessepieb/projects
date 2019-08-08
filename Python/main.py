b34404b00b9a51b60c188b7182f82221a0e7d000
Jessepieb <jessepiebenga@gmail.com>
2019-08-08 02:26:12 +0200

    up

--- Python/main.py
+++ Python/main.py
@@ -1,52 +1,33 @@
 import smtplib
 import nmap
-import os
+
+file = open("input.txt")
+creds = file.readlines()
+sent_from = creds[0]
+to = [creds[0]]
+nmScan = nmap.PortScanner()
+subject = "Testmail"
+msg = """"""
+nmScan.scan(hosts='192.168.192.1/24', arguments=' -O')
+print(nmScan.csv())
+for host in nmScan.all_hosts():
+    msg += str(host)
+    #msg += str(nmScan[host].get())
+email_text= """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from,", ".join(to), subject, msg)
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(creds[0], creds[1])
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+except:
+    print("somthing went wrong")
 
 
-def getMacs(iprange):
-
-    macs = open("macs.txt", "a+")
-    nmScan = nmap.PortScanner()
-    nmScan.scan(hosts=iprange, arguments='-sn')
-    msg = ""
-    for host in nmScan.all_hosts():
-        if 'mac' in nmScan[host]['addresses']:
-            msg += str(nmScan[host]['addresses']['ipv4']) + ' : ' + str(nmScan[host]['vendor'])
-            if str(nmScan[host]['vendor']) not in macs:
-                macs.write(str(nmScan[host]['vendor']) + '\n')
-        msg += nmScan[host].hostname() + '\n '
-
-    macs.close()
-    return msg
-
-def sendMail(body):
-
-    file = open("input.txt")
-    creds = file.readlines()
-    sent_from = creds[0]
-    to = [creds[0]]
-    msg = body
-    email_text = """\
-    From: %s
-    To: %s
-    
-    %s
-    """ % (sent_from, ",".join(to), str(msg))
-    try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.ehlo()
-        server.login(creds[0], creds[1])
-        server.sendmail(sent_from, to, email_text)
-        server.close()
-    except:
-        print("somthing went wrong")
-    file.close()
-
-
-def main():
-    text = getMacs('192.168.192.1/24')
-    sendMail(text)
-    os.remove("macs.txt")
-
-
-main()
+file.close()
