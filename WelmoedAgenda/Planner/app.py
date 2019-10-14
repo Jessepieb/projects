2afa70735ae1d067761d316e9bd8ab7456e2b7f9
Jessepieb <jessepiebenga@gmail.com>
2019-10-14 23:16:51 +0200

    basicWebApp

--- WelmoedAgenda/Planner/app.py
+++ WelmoedAgenda/Planner/app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+import os
+import sys
+import flask
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, folder)
 
-app = Flask(__name__)
+
+from infrastructure.view_modifiers import response
+
+app = flask.Flask(__name__)
+
+
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
+    ]
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@response(template_file='home/index.html')
+def index():
+    test_packages = get_latest_packages()
+    return {'packages': test_packages}
+    # return flask.render_template('home/index.html', packages=test_packages)
+
+
+@app.route('/about')
+@response(template_file='home/about.html')
+def about():
+    return {}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
