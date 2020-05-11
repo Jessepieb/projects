c2768c1c3ed9dff0744dcc8d432ca0afef77929f
Jessepieb <jessepiebenga@gmail.com>
2020-05-11 13:40:53 +0200

    Revert "refactor"
    This reverts commit 5a83a799921f78c13e120646051dcbbfc576598a.

--- Graphics/biljart/Table.js
+++ Graphics/biljart/Table.js
@@ -1,7 +1,7 @@
 class Table {
     constructor(width, height, scene) {
         this._widthScale = 1;//(width/1920);
-        this._heightScale = (height / 1080);
+        this._heightScale = (height/1080);
         this._width = width;
         this._height = height;
         this.scene = scene;
@@ -12,12 +12,12 @@ class Table {
         this.base = this.createBase();
     }
 
-    createBase() {
+    createBase(){
         const w = this._widthScale * 50;
         const h = this._heightScale * 30;
         const geometry = new THREE.PlaneGeometry(w, h);
-        const material = new THREE.MeshBasicMaterial({color: 'forestgreen'});
-        const mesh = new THREE.Mesh(geometry, material);
+        const material = new THREE.MeshBasicMaterial({color:'forestgreen'});
+        const mesh = new THREE.Mesh(geometry,material);
         this.scene.add(mesh);
         this.initTable();
 
@@ -25,16 +25,16 @@ class Table {
     }
 
 
-    createWalls() {
+    createWalls(){
         var w = this._widthScale * 2.35;
         var l = this._heightScale * 60;
-        const wall1 = new Wall(0, this._height / 73, l, w);
-        const wall2 = new Wall(0, (this._height / 73) * -1, l, w);
+        const wall1 = new Wall(0,this._height/73,l,w);
+        const wall2 = new Wall(0,(this._height/73)*-1,l,w);
 
-        l = this._heightScale * 27;
+        l = this._heightScale* 27;
 
-        const wall3 = new Wall((this._width / 75) * -1, 0, w, l);
-        const wall4 = new Wall(this._width / 75, 0, w, l);
+        const wall3 = new Wall((this._width/75)*-1,0,w,l);
+        const wall4 = new Wall(this._width/75,0,w,l);
 
         this.walls.add(wall1.mesh);
         this.walls.add(wall2.mesh);
@@ -45,17 +45,17 @@ class Table {
     }
 
 
-    createPockets() {
-        for (let i = -1; i < 2; i++) {
-            var pocket = new Pocket((this._width / 78) * i, this._height / 77, 2);
+    createPockets(){
+        for(let i = -1; i < 2; i++) {
+            var pocket = new Pocket((this._width/78)*i,this._height/77,2);
             this.pockets.add(pocket.mesh);
-            pocket = new Pocket((this._width / 78) * i, (this._height / 77) * -1, 2);
+            pocket = new Pocket((this._width/78)*i,(this._height/77)*-1,2);
             this.pockets.add(pocket.mesh)
         }
         this.scene.add(this.pockets);
     }
 
-    initTable() {
+    initTable(){
         this.createWalls();
         this.createPockets();
     }
