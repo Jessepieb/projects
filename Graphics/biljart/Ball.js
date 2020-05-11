156a571fc706d23b458fa43f14b3539ac7016b0d
Jessepieb <jessepiebenga@gmail.com>
2020-05-11 13:40:57 +0200

    Revert "update"
    This reverts commit 53dcbb3db488ae3e2d4adab004aec1c3ad2802b0.

--- Graphics/biljart/Ball.js
+++ Graphics/biljart/Ball.js
@@ -2,7 +2,7 @@ class Ball {
     constructor(x,y,radius,txtId, scene) {
         this.scene = scene;
         this._location = new THREE.Vector2(x,y);
-        // this.txtId = txtId;
+        this.txtId = txtId;
         this._radius = radius;
 
         this._velocity = new THREE.Vector2(0,0);
@@ -10,13 +10,13 @@ class Ball {
         // this.texture = new THREE.TextureLoader().load('models/textures/Ball' + this.txtId + '.jpg');
 
         this.geometry = new THREE.SphereGeometry(this._radius, 32, 32);
-        this.material = new THREE.MeshBasicMaterial({color:"white"});
+        this.material = new THREE.MeshBasicMaterial({color:0x000000});
         // this.material = new THREE.MeshBasicMaterial({map:this.texture});
 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.position.x = this._location.x;
         this.mesh.position.y = this._location.y;
-        // this.mesh.position.z = 2;
+        this.mesh.position.z = 2;
 
         this.scene.add(this.mesh);
     }
