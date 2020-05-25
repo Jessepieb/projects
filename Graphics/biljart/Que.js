d1a299b9de967037fc10e43fac9495bc2e80d6b9
Jessepieb <jessepiebenga@gmail.com>
2020-05-25 11:54:04 +0200

    update

--- Graphics/biljart/Que.js
+++ Graphics/biljart/Que.js
@@ -3,33 +3,27 @@ class Que{
         this._scene = scene;
         this._location = new THREE.Vector2(x,y);
 
-        this.points = [];
-        this.points.push( new THREE.Vector2(this._location.x, this._location.y) );
-        this.points.push( new THREE.Vector2( 10, 0) );
 
+        this._material = new THREE.MeshBasicMaterial({color:'red'});
+        this.geometry = new THREE.SphereBufferGeometry(0.5,32,32);
 
-        this._material = new THREE.LineBasicMaterial({color:'red'});
-        this.geometry = new THREE.BufferGeometry().setFromPoints(this.points);
-
-        this.geometry.verticesNeedUpdate = true;
-
-        this.mesh = new THREE.Line(this.geometry,this._material);
+        this.mesh = new THREE.Mesh(this.geometry,this._material);
         this.mesh.name = "Que";
         this._scene.add(this.mesh);
     }
 
     set location(newLoc){
         this._location = newLoc;
+        this.updatepos();
     }
 
-    updatePos(ball,mouse){
-       this.points = [];
-       this.points.push(new THREE.Vector2(ball.x,ball.y));
-       this.points.push(new THREE.Vector2(mouse.x,mouse.y));
-
-       this.mesh.geometry = new THREE.BufferGeometry().setFromPoints(this.points);
-
-       this.mesh.geometry.verticesNeedUpdate = true;
+    updatepos(){
+       // this.points = [];
+       // this.points.push(new THREE.Vector2(ball.x,ball.y));
+       // this.points.push(new THREE.Vector2(mouse.x,mouse.y));
+       // this.mesh.geometry = new THREE.BufferGeometry().setFromPoints(this.points);
+        this.mesh.position.x = this._location.x;
+        this.mesh.position.y = this._location.y;
     }
 
 
