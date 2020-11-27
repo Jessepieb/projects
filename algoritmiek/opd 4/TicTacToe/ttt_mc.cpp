9b60f2d62a94671c60647ec7ee37217e422cf0d4
Jessepieb <jessepiebenga@gmail.com>
2020-11-27 13:00:49 +0100

    up

--- algoritmiek/opd 4/TicTacToe/ttt_mc.cpp
+++ algoritmiek/opd 4/TicTacToe/ttt_mc.cpp
@@ -7,7 +7,7 @@
 #include <iterator>
 #include "ttt.h"
 
-unsigned const n_trials = 1500;
+unsigned const n_trials = 2500;
 unsigned const mc_match = 1;
 unsigned const mc_other = 1;
 
@@ -15,32 +15,78 @@ enum class PlayerType { Human, Computer };
 
 State mcTrial(const State& board)
 {
-
-}
-
-Move mcMove(const State& board, const Player& player)
-{
-	
+	State b = board;
+	while (getMoves(b).size() > 0) {
+		std::vector<Move> posMoves = getMoves(b);
+		auto randMove = select_randomly(posMoves.begin(), posMoves.end());
+		b = doMove(b, *randMove);
+		if (getWinner(b) != Player::None) {
+			break;
+		}
+	}
+	return b;
 }
 
 void mcUpdateScores(std::array<int, 9>& scores, const State& board, const Player& player)
 {
+	if (getWinner(board) == player) {
+		for (int i = 0; i < board.size(); i++)
+		{
+			if (board[i] == player)scores[i] += mc_match;
+			else if (board[i] != Player::None)scores[i] -= mc_other;
+		}
+	}
+	else if (getWinner(board) != player)
+	{
+		for (int i = 0; i < board.size(); i++)
+		{
+			if (board[i] == player) scores[i] -= mc_match;
+			else if (board[i] != Player::None) scores[i] += mc_other;
+		}
+	}
+
 }
 
-
-Move getBestMove(const State& board)
+Move getBestMove(const std::array<int, 9>& scores, const State& board)
 {
+	std::vector<Move> possibleMoves = getMoves(board);
+	std::vector<Move> bestMove{ 0 };
+	if (possibleMoves.size() == 1) {
+		return possibleMoves[0];
+	}
 
+	for (int i = 0; i < possibleMoves.size(); i++)
+	{
+		if (scores[possibleMoves[i]] > scores[bestMove[0]]) {
+			bestMove.clear();
+			bestMove.push_back(possibleMoves[i]);
+		}
+		else if (scores[possibleMoves[i]] == bestMove[0]) {
+			bestMove.push_back(possibleMoves[i]);
+		}
+	}
+	return *select_randomly(bestMove.begin(), bestMove.end());
 }
 
+Move mcMove(const State& board, const Player& player)
+{
+	std::array<int, 9> boardScores{ 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	for (int i = 0; i <  n_trials; i++)
+	{
+		State trail = board;
+		trail = mcTrial(trail);
+		mcUpdateScores(boardScores, trail, getCurrentPlayer(trail));
+	}
+	return getBestMove(boardScores, board);
+}
 
 int main()
 {
 	std::srand(std::time(0));
 
 	std::map<Player, PlayerType> playerType;
-	playerType[Player::O] = PlayerType::Human;
-	playerType[Player::X] = PlayerType::Computer;
+	playerType[Player::O] = PlayerType::Computer;
+	playerType[Player::X] = PlayerType::Human;
 
 	State board = {
 		Player::None, Player::None, Player::None,
