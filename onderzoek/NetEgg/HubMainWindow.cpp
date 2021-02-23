1c7b680f0fc231d57bb4d56260ebea5ac6c203c0
Jessepieb <jessepiebenga@gmail.com>
2021-02-23 22:45:19 +0100

    update

--- onderzoek/NetEgg/HubMainWindow.cpp
+++ onderzoek/NetEgg/HubMainWindow.cpp
@@ -8,8 +8,8 @@ HINSTANCE hInst;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int CALLBACK WinMain(
-	_In_ HINSTANCE hInstance,
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,	
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nCmdShow
