af83c528d2e2c6a03ff2aad545f83ad8de5abc9b
Jessepieb <jessepiebenga@gmail.com>
2021-05-13 16:07:45 +0200

    update
    added setting manager base, added json functionality

--- concurrent/final/opd4/Main.cpp
+++ concurrent/final/opd4/Main.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
-#include <fstream>
-#include <thread>
-#include <mutex>
-#include <cstdio>
-#include <string>
-#include <filesystem>
+#include "Main.h"
 
 namespace fs = std::filesystem;
+
+//std::multimap<std::string, std::string> criteria;
+//std::multimap<std::string, std::string>::iterator it;
+
+//criteria.insert(std::pair<std::string, std::string>("Python", "python"));
+
+//for (it = criteria.begin(); it != criteria.end(); it++)
+//{
+//	std::cout << (*it).first << "\t" << (*it).second << std::endl;
+//}
+
 
 class FileHandler {
 public:
@@ -40,21 +45,95 @@ public:
 			dest.close();
 	}
 
+	void find_keywords() {
+
+	}
+
 private:
 	
 	
 };
 
+std::string tolowerString(std::string text) {
+	std::string newString;
+	std::transform(text.begin(), text.end(),std::back_inserter(newString), (int(*)(int))std::tolower);
+	std::cout << newString << std::endl;
+	return newString;
+}
 
+class Settings_Manager
+{
+	using json = nlohmann::json;
+public:
+	Settings_Manager();
+	~Settings_Manager();
+
+	void create_loc() {};
+	void remove_loc() {};
+
+	void add_keywords() {};
+	void remove_keywords() {};
+
+	void add_extensions() {};
+	void remove_extensions() {};
+
+private:
+	json j_object;
+	std::ifstream setting_in;
+	std::ofstream setting_out;
+};
+
+Settings_Manager::Settings_Manager()
+{
+	if (fs::exists("settings.json")) {
+		setting_in.open("settings.json");
+		setting_in >> j_object;
+		std::cout << j_object.dump(4);
+	}
+	else {
+		std::cout << "Couldn't find settings.json, creating new settings file" << std::endl;
+		setting_out.open("settings.json");
+		j_object = { {"locations",{}} };
+		std::cout << j_object << std::endl;
+		setting_out << std::setw(4) << j_object << std::endl;
+	}
+	
+}
+
+Settings_Manager::~Settings_Manager()
+{
+	if (setting_in.is_open()) {
+		setting_in.close();
+	}
+
+	if (setting_out.is_open()) {
+		setting_out.close();
+	}
+
+}
 
 
 int main() {
+
+	Settings_Manager sm;
+
 	FileHandler fh;
-	fh.write_file("test.txt");
-	fh.copy_file("test.txt", "text2.txt");
+	fh.write_file("text.txt");
+	fh.copy_file("text.txt", "text2.txt");
+
+	int counter = 0;
 
 	for (auto& i : fs::directory_iterator("D:/Github/projects/concurrent/final/opd4"))
 	{
-		std::cout << i.path() << std::endl;
+		if (i.is_regular_file()) {
+			std::cout << i.path().filename() << std::endl;
+			std::string::size_type pos = i.path().string().find(tolowerString("TeXt"));
+			if (pos != std::string::npos) {
+				std::cout << "Contains keyword!\n";
+			}
+			counter++;
+		}	
 	}
+
+	std::cout << "amount of files in directory: " << counter << std::endl;
 }
---
+++ concurrent/final/opd4/Main.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef MAIN_H 
+#define MAIN_H
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <thread>
+#include <mutex>
+#include <cstdio>
+#include <string>
+#include <map>
+#include <filesystem>
+#include "json.hpp"
+
+#endif
+
